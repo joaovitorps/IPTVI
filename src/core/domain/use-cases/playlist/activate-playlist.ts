1f3f9130a91ee0a2e71f91f2950ecf7aca7f6b82
Joao Santos <dev.joaosantos@outlook.com>
2026-05-20 11:56:58 -0300

    feat: validate credentials before activating playlist

--- src/core/domain/use-cases/playlist/activate-playlist.ts
+++ src/core/domain/use-cases/playlist/activate-playlist.ts
@@ -1,20 +1,43 @@
+import { CredentialRepository } from "../../repositories/credential-repository";
 import { PlaylistRepository } from "../../repositories/playlist-repository";
 import { EntityNotFoundError } from "../error/entity-not-found-error";
+import { InvalidCredentialsError } from "../error/invalid-credentials-error";
 
 interface ActivatePlaylistUseCaseParams {
   playlistId: string;
 }
 
-export class ActivatePlaylistUseCase {
-  constructor(private readonly playlistRepository: PlaylistRepository) {}
+interface ActivatePlaylistUseCaseReturn {
+  activated: boolean;
+}
 
-  async execute({ playlistId }: ActivatePlaylistUseCaseParams): Promise<void> {
+export class ActivatePlaylistUseCase {
+  constructor(
+    private readonly playlistRepository: PlaylistRepository,
+    private readonly credentialRepository: CredentialRepository,
+  ) {}
+
+  async execute({
+    playlistId,
+  }: ActivatePlaylistUseCaseParams): Promise<ActivatePlaylistUseCaseReturn> {
     const playlist = this.playlistRepository.getById(playlistId);
 
     if (!playlist) {
       throw new EntityNotFoundError();
     }
 
+    // Validate credentials BEFORE activating
+    const validation = await this.credentialRepository.validate({
+      server: playlist.server,
+      username: playlist.username,
+      password: playlist.password,
+    });
+
+    if (!validation.ok) {
+      throw new InvalidCredentialsError(validation.data.error);
+    }
+
+    // Deactivate all other playlists first
     const activePlaylists = await this.playlistRepository.fetchActives();
 
     for (const activePlaylist of activePlaylists) {
@@ -24,7 +47,10 @@ export class ActivatePlaylistUseCase {
       }
     }
 
+    // Activate the target playlist
     playlist.isActive = true;
     await this.playlistRepository.save(playlist);
+
+    return { activated: true };
   }
 }
