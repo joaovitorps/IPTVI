9b251e6936ebc09af777139dd4e73bc0c1bece14
Joao Santos <dev.joaosantos@outlook.com>
2026-05-20 11:58:09 -0300

    fix: pass credential repository to activate playlist handler

--- src/main/handlers/activate-playlist.ts
+++ src/main/handlers/activate-playlist.ts
@@ -1,7 +1,11 @@
+import { APICredentialRepository } from "@/core/domain/repositories/api/api-credential-repository";
 import { StorePlaylistRepository } from "@/core/domain/repositories/store/store-playlist-repository";
 import { ActivatePlaylistUseCase } from "@/core/domain/use-cases/playlist/activate-playlist";
 
 export const activatePlaylist = async (playlistId: string) => {
-  const useCase = new ActivatePlaylistUseCase(new StorePlaylistRepository());
-  await useCase.execute({ playlistId });
+  const useCase = new ActivatePlaylistUseCase(
+    new StorePlaylistRepository(),
+    new APICredentialRepository(),
+  );
+  return useCase.execute({ playlistId });
 };
